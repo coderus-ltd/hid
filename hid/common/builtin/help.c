468c150570581b6b0b70bcaa4c65369c0bb7064c
Eddie Woodley <edward.woodley@coderus.com>
2014-11-24 14:47:47 +0000

    doc(setreport): remove line about interface changing from help text

--- hid/common/builtin/help.c
+++ hid/common/builtin/help.c
@@ -85,7 +85,6 @@ int cmd_help(int argc, const char **argv)
               "hid setreport allows you to send a report to an attached hid device.\n"
               "usage: hid setreport [--help] [-t output|feature] [-i <id>] [-w <time>] [-r] [-o] [-x] -d <data> [data...]\n"
               "\n"
-              "This command is a WIP and therefore is incomplete and may change.\n"
               "If more than one device is matched, the first-discovered matching device is used.\n"
               "\n"
               "Arguments:\n"

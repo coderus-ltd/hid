3aeac577b75c6cf2c7b2ddecb370b83be24500e7
Troy Rijkaard <troy.rijkaard@coderus.com>
2014-11-27 10:11:07 +0000

    feat(attached): add method for attached file

--- hid/win/builtin/attached.cpp
+++ hid/win/builtin/attached.cpp
@@ -5,7 +5,27 @@ extern "C"
     #include "builtin.h"
 }
 
+/* Members */
+static HidManager hid_manager;
+static bool deviceAttached;
+
+/* Methods */
+static int attached_execution_block(std::wstring device, bool* foundDevice)
+{
+    deviceAttached = true;
+    return 0;
+}
+
 int cmd_attached(int argc, const char**argv)
 {
-    return 0;
+    deviceAttached = false;
+
+    int ret = hid_manager.Process_Devices(argc, argv, &attached_execution_block);
+
+    if (deviceAttached == 0)
+        std::cout << 0;
+    else
+        std::cout << 1;
+
+    return ret;
 }

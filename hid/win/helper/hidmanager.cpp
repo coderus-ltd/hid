3d3b98277ead30da4a97a4afb6b490d399159349
Troy Rijkaard <troy.rijkaard@coderus.com>
2014-11-27 12:12:36 +0000

    feat(manager): add ability to filter devices found by their vid/pid

--- hid/win/helper/hidmanager.cpp
+++ hid/win/helper/hidmanager.cpp
@@ -6,6 +6,7 @@
 
 #include "hidmanager.h"
 
+// Get the path to the device, used to create a device handle
 static std::wstring Get_Device_Path(HDEVINFO hInfoSet, SP_DEVICE_INTERFACE_DATA oInterface)
 {
     std::wstring retvalue;
@@ -34,10 +35,11 @@ static std::wstring Get_Device_Path(HDEVINFO hInfoSet, SP_DEVICE_INTERFACE_DATA 
     return retvalue;
 }
 
+// Find where null termination occurs in the buffer
 static int Find_Null_Termination(unsigned char buffer[], std::wstring searchString)
 {
     int retvalue = 0;
-    for (int i = 0; i < searchString.length(); i++)
+    for (unsigned int i = 0; i < searchString.length(); i++)
     {
         if (buffer[i] == '\0' && buffer[i + 1] == '\0')
         {
@@ -46,6 +48,87 @@ static int Find_Null_Termination(unsigned char buffer[], std::wstring searchStri
     }
 
     return retvalue;
+}
+
+// Change a string to a wide string
+static std::wstring String_To_Wide_String(const std::string& str)
+{
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+    std::wstring wstrTo(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+
+    return wstrTo;
+}
+
+// Get the data for the paramType (-v vid, -p pid)
+static std::wstring Get_Param(int argc, const char **argv, std::string paramType)
+{
+    const char* paramData = '\0';
+
+    for (int i = 0; i < argc; i++)
+    {
+        if (i + 1 != argc)
+        {
+            if (argv[i] == paramType)
+            {
+                paramData = argv[i + 1];
+                break;
+            }
+        }
+    }
+
+    if (paramData != '\0')
+    {
+        std::string ret(paramData);
+        return String_To_Wide_String(ret);
+    }
+    else
+    {
+        return L"";
+    }
+}
+
+// Format is vid_0000&pid_0000
+static std::wstring Get_Search_String(int argc, const char **argv)
+{
+    std::wstring ret;
+    std::wstring vid = Get_Param(argc, argv, "-v");
+    std::wstring pid = Get_Param(argc, argv, "-p");
+
+    if (!vid.empty() && !pid.empty())
+    {
+        ret = L"vid_" + vid + L"&pid_" + pid;
+    }
+    else if (!vid.empty() && pid.empty())
+    {
+        ret = L"vid_" + vid;
+    }
+    else if (vid.empty() && !pid.empty())
+    {
+        ret = L"pid_" + pid;
+    }
+
+    return ret;
+}
+
+// Compare the devicePath and searchLocation strings to see if we should include it in the found devices
+static bool Compare_Path_And_Search_Location(std::wstring devicePath, std::wstring searchLocation)
+{
+    bool ret;
+    if (devicePath.empty())
+    {
+        ret = false;
+    }
+    else if (searchLocation.empty())
+    {
+        ret = true;
+    }
+    else
+    {
+        ret = devicePath.find(searchLocation) != std::string::npos;
+    }
+
+    return ret;
 }
 
 HidManager::HidManager()
@@ -70,6 +153,7 @@ int HidManager::Process_Devices(int argc, const char **argv, ProcessDeviceBlock 
     int result = 0;
     int nIndex = 0;
 
+    std::wstring searchString = Get_Search_String(argc, argv);
     std::vector<std::wstring> foundDevices;
 
     SP_DEVICE_INTERFACE_DATA oInterface;
@@ -78,7 +162,7 @@ int HidManager::Process_Devices(int argc, const char **argv, ProcessDeviceBlock 
     while (SetupDiEnumDeviceInterfaces(hInfoSet, NULL, &guid, nIndex, &oInterface))
     {
         std::wstring strDevicePath = Get_Device_Path(hInfoSet, oInterface);
-        if (!strDevicePath.empty())
+        if (Compare_Path_And_Search_Location(strDevicePath, searchString))
         {
             foundDevices.push_back(strDevicePath);
         }
@@ -89,7 +173,7 @@ int HidManager::Process_Devices(int argc, const char **argv, ProcessDeviceBlock 
     bool stopLoop = false;
     if (foundDevices.size() > 0)
     {
-        for (int i = 0; i < foundDevices.size() && !stopLoop; i++)
+        for (unsigned int i = 0; i < foundDevices.size() && !stopLoop; i++)
         {
             result = execution_block(foundDevices[i], &stopLoop);
         }
@@ -182,7 +266,7 @@ std::wstring HidManager::Get_Serial_String(HANDLE handle)
 std::wstring HidManager::clean_string(std::wstring old_string)
 {
     std::wstring ret;
-    for (int i = 0; i < old_string.length(); i++)
+    for (unsigned int i = 0; i < old_string.length(); i++)
     {
         if (old_string[i] != '\0')
         {

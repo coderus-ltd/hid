b8d7a042026ce01cb2b8c670bccff9b0469fb30c
Troy Rijkaard <troy.rijkaard@coderus.com>
2014-12-03 09:59:28 +0000

    feat(reports): add basic code for the set and get reports

--- hid/win/builtin/getreport.cpp
+++ hid/win/builtin/getreport.cpp
@@ -1,10 +1,27 @@
 #include "hidmanager.h"
+#include "hiddevice.h"
 
 extern "C"
 {
     #include "builtin.h"
 }
+
+static int getreport_execution_block(std::wstring device, bool* foundDevice)
+{
+    *foundDevice = true;
+    HidDevice hid_device(device);
+    HANDLE handle = hid_device.get_device_handle(device);
+
+    if (handle != 0 && handle != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(handle);
+    }
+
+    return 0;
+}
+
 int cmd_getreport(int argc, const char **argv)
 {
-    return 0;
+    HidManager hid_manager;
+    return hid_manager.process_devices(argc, argv, &getreport_execution_block);
 }

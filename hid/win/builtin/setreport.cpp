2396f9236e914b55044892a57d674966e46d4cd4
Aquilino Viveiros <aquilino.viveiros@coderus.com>
2014-12-10 09:15:16 +0000

    feat(setreport): added support for feature report type on the setreport command

--- hid/win/builtin/setreport.cpp
+++ hid/win/builtin/setreport.cpp
@@ -13,8 +13,28 @@ extern "C"
 }
 
 /* Members */
+static std::wstring reportType = L"output";
+static int reportId = 1;
 static int waitTime = 0;
-static std::vector<std::wstring> commands;
+static std::vector<std::string> commands;
+
+static std::string dataFromHexString(std::string hexString) {
+	std::string _hex = hexString.substr(0, 2) == "0x" ? hexString.substr(2, hexString.length()) : hexString;
+	int i = 0, byte = 0, len = _hex.length();
+	const int charsLength = (len / 2) + 1 /*+1 = \0*/;
+	char *chars = new char[charsLength];
+	while (i < len)
+	{
+		std::string _hexByteStr = _hex.substr(i, 2);
+		int decVal = std::stoul(_hexByteStr, nullptr, 16);
+		chars[byte] = decVal;
+		i = +2;
+		byte++;
+	}
+	chars[byte] = 0;
+
+	return std::string(chars);
+}
 
 static char* readData(HANDLE& handle, HidDevice hid_device) {
 	char* data = NULL;
@@ -32,22 +52,26 @@ static char* readData(HANDLE& handle, HidDevice hid_device) {
 		overlap.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 		HANDLE ev = overlap.hEvent;
 
-		if (!ReadFile(handle, outputBuffer, outputBufferSize, &dwRead, &overlap)) {
-			if (GetLastError() != ERROR_IO_PENDING) {
+		if (!ReadFile(handle, outputBuffer, outputBufferSize, &dwRead, &overlap))
+		{
+			if (GetLastError() != ERROR_IO_PENDING)
+			{
 				std::cout << "not io pending" << std::endl;
 				CloseHandle(ev);
 				return 0;
 			}
 		}
 
-		res = WaitForSingleObject(ev, waitTime == 0 ? 100 : waitTime * 1000);
-		if (res != WAIT_OBJECT_0) {
+		res = WaitForSingleObject(ev, waitTime == 0 ? 1000 : waitTime * 1000);
+		if (res != WAIT_OBJECT_0)
+		{
 			CloseHandle(ev);
 			return 0;
 		}
 
 		res = GetOverlappedResult(handle, &overlap, &dwRead, TRUE);
-		if (res && dwRead > 0) {
+		if (res && dwRead > 0)
+		{
 			data = new char[outputBufferSize];
 			memcpy(data, &outputBuffer[1], (size_t)(outputBufferSize > dwRead ? dwRead : outputBufferSize));
 		}
@@ -65,28 +89,33 @@ static int readReportData(HANDLE& handle, HidDevice hid_device) {
 
 	//start clock
 	beginTime = clock();
-	while (true) {
+	while (true)
+	{
 		//compute elapsed time
 		unsigned long elapsedTime = ((unsigned long)clock() - beginTime) / CLOCKS_PER_SEC;
-		if (elapsedTime > seconds) {
+		if (elapsedTime > seconds)
+		{
 			break;
 		}
 
 		char* data = readData(handle, hid_device);
-		if (data == NULL) { //no more data
+		if (data == NULL)
+		{ //no more data
 			break;
 		}
-		else 
+		else
 		{ //got data
 			dataValues.push_back(data);
-			if (dataValues.size() == 20) {
+			if (dataValues.size() == 20)
+			{
 				break;
 			}
 		}
 	}
 
 	bool lineBreak = dataValues.size() > 1;
-	for (char* dataValue : dataValues) {
+	for (char* dataValue : dataValues)
+	{
 		lineBreak ? std::cout << dataValue << std::endl : std::cout << dataValue;
 	}
 
@@ -102,37 +131,48 @@ static int setreport_execution_block(std::wstring device, bool* foundDevice)
 
 	if (handle != 0 && handle != INVALID_HANDLE_VALUE)
 	{
-		for (std::wstring command : commands) {
+		for (std::string command : commands)
+		{
 			const size_t reportDataSize = command.size() + 1; //+1 - termination character (\0)
-			const size_t sendingReportSize = caps.OutputReportByteLength;
+			const size_t sendingReportSize = caps.OutputReportByteLength == 0 ? 0x31 : caps.OutputReportByteLength;
 
-			if (reportDataSize > sendingReportSize) {
+			if (reportDataSize > sendingReportSize)
+			{
 				std::cout << "warn: report larger than maximum size" << std::endl;
 			}
 
 			//prepare report data	
 			char* reportData = new char[reportDataSize];
 
-			// conversion
-			size_t i;
-			wcstombs_s(&i, reportData, reportDataSize, command.c_str(), reportDataSize);
+			// copy report data to char array
+			memcpy(reportData, command.c_str(), command.size());
+			reportData[command.size()] = 0;
 
 			//prepare report
 			char* reportBuffer = new char[sendingReportSize];
 
 			// set report id
-			reportBuffer[0] = 0x01;
+			reportBuffer[0] = reportId;
 
-			// copy data into the report buffer
+			// copy data
 			strncpy_s(&reportBuffer[1], sendingReportSize, reportData, sendingReportSize);
-
-			// send the report
-			if (HidD_SetOutputReport(handle, reportBuffer, sendingReportSize)) {
-				readReportData(handle, hid_device); //read report
-			}
-			else
+			if (reportType == L"output")
 			{
-				std::cout << "setreport: send error (code: " << GetLastError() << ")" << std::endl;
+				if (HidD_SetOutputReport(handle, reportBuffer, sendingReportSize))
+				{
+					readReportData(handle, hid_device); //read report
+				}
+				else
+				{
+					std::cout << "setreport: send error (code: " << GetLastError() << ")" << std::endl;
+				}
+			}
+			else if (reportType == L"feature")
+			{
+				if (!HidD_SetFeature(handle, reportBuffer, sendingReportSize))
+				{
+					std::cout << "setreport: send error (code: " << GetLastError() << ")" << std::endl;
+				}
 			}
 		}
 
@@ -144,9 +184,11 @@ static int setreport_execution_block(std::wstring device, bool* foundDevice)
 
 int cmd_setreport(int argc, const char **argv)
 {
-	if (_isatty(_fileno(stdin))) { //stdin is a terminal
+	if (_isatty(_fileno(stdin)))
+	{ //stdin is a terminal
 		//lookup -d
-		for (int i = 1; i < argc; i++) {
+		for (int i = 1; i < argc; i++)
+		{
 			if (strcmp(argv[i], "-d") == 0)
 			{
 				for (int j = i + 1; j < argc; j++)
@@ -158,11 +200,12 @@ int cmd_setreport(int argc, const char **argv)
 
 					//parse commands
 					const char* str = argv[j];
-					std::wstring data(str, str + strlen(str));
-					if (data.length() % 2 == 0 && data.find(L"0x") == 0) {
-						commands.push_back(data.substr(2, data.length()));
+					std::string data(str);
+					if (data.length() % 2 == 0 && data.substr(0, 2) == "0x")
+					{
+						commands.push_back(dataFromHexString(data));
 					}
-					else 
+					else
 					{
 						commands.push_back(data);
 					}
@@ -172,24 +215,39 @@ int cmd_setreport(int argc, const char **argv)
 			}
 		}
 	}
-	else 
+	else
 	{ //stdin is a file or a pipe
 		std::string inputStr;
 		std::cin >> inputStr;
-		std::wstring input(inputStr.begin(), inputStr.end());
-		if (!input.empty()) {
+		std::string input(inputStr.begin(), inputStr.end());
+		if (!input.empty())
+		{
 			commands.push_back(input);
 		}
 	}
-	
-	if (commands.size() == 0) {
+
+	if (commands.size() == 0)
+	{
 		return 0;
 	}
-	
+
 	std::wstring _waitTime = get_w_param(argc, argv, "-w");
-	if (!_waitTime.empty()) {
+	if (!_waitTime.empty())
+	{
 		int wt = std::stoi(_waitTime);
 		waitTime = wt > 0 ? wt : 0;
+	}
+
+	std::wstring _reportType = get_w_param(argc, argv, "-t");
+	if (!_reportType.empty())
+	{
+		reportType = _reportType;
+	}
+
+	std::wstring _reportId = get_w_param(argc, argv, "-i");
+	if (!_reportId.empty())
+	{
+		reportId = std::stoul(_reportId, nullptr, 16);
 	}
 
 	HidManager hid_manager;

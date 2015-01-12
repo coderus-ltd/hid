83f56f20eb753ebd6e4e764426b2a0e8137f8cff
Aquilino Viveiros <aquilino.viveiros@coderus.com>
2015-01-12 11:06:39 +0000

    fix(build): fix issue #12 on the version header

--- hid/win/prebuild.bat
+++ hid/win/prebuild.bat
@@ -1,6 +1,8 @@
+rem "Only works with UK date format"
+
 @echo off
 for /f "delims=" %%a in ('git describe --tags --always') do @set _GIT_DESCRIBE=%%a
-for /f "delims=" %%b in ('echo %date:~10,4%-%date:~7,2%-%date:~4,2% %time:~0,8%') do @set _DATE=%%b
+for /f "delims=" %%b in ('echo %date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,8%') do @set _DATE=%%b
 echo #ifndef VERSION > ../common/version.h
 echo #define VERSION "hid version %_GIT_DESCRIBE%\nBuilt %_DATE%" >> ../common/version.h
 echo #endif VERSION >> ../common/version.h
